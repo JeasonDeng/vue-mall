69db9267bbd4e70f9b20772668d80d4a5f85d009
jeasonDeng <m13871237955@163.com>
2019-11-12 16:28:05 +0800

    第三次提交

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import Vue from 'vue'
 import './lib/css/mui.min.css'
 import './lib/css/icons-extra.css'
 
-import { Header } from 'mint-ui'
+import { Header, Swipe, SwipeItem } from 'mint-ui'
 
 Vue.component(Header.name, Header)
+Vue.component(Swipe.name, Swipe)
+Vue.component(SwipeItem.name, SwipeItem)
 
 import App from './App'
 import router from './router'
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,117 @@
 <template>
   <div>
-    Home
+    <mt-swipe>
+      <mt-swipe-item v-for="(item, i) in lunbotu" :key="i">
+        <img :src="item.img" />
+      </mt-swipe-item>
+    </mt-swipe>
+
+    <ul class="mui-table-view mui-grid-view mui-grid-9">
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../images/menu1.png" alt />
+          <div class="mui-media-body">Home</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../images/menu2.png" alt />
+          <div class="mui-media-body">Email</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../images/menu3.png" alt />
+          <div class="mui-media-body">Chat</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../images/menu4.png" alt />
+          <div class="mui-media-body">location</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../images/menu5.png" alt />
+          <div class="mui-media-body">Search</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../images/menu6.png" alt />
+          <div class="mui-media-body">Phone</div>
+        </a>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {
+import axios from 'axios'
+import Toast from 'mint-ui'
 
+axios.defaults.baseURL = 'http://www.liulongbin.top:3005'
+
+export default {
+  data() {
+    return {
+      lunbotu: []
+    }
+  },
+  created() {
+    this.getlunbo()
+  },
+  methods: {
+    getlunbo() {
+      axios.get('api/getlunbo')
+        .then(res => {
+          if (res.data.status === 0) {
+            this.lunbotu = res.data.message
+          } else {
+            Toast('获取轮播图数据失败!')
+          }
+        })
+    }
+  }
 }
 </script>
 
-<style scoped>
-
+<style lang="less" scoped>
+.mint-swipe {
+  height: 200px;
+  .mint-swipe-item {
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
+.mui-grid-view.mui-grid-9 {
+  background-color: #fff;
+  border: 0;
+  .mui-table-view-cell {
+    border: 0;
+    padding-bottom: 10px;
+    &:nth-child(4) {
+      padding-top: 0;
+    }
+    &:nth-child(5) {
+      padding-top: 0;
+    }
+    &:nth-child(6) {
+      padding-top: 0;
+    }
+    img {
+      width: 60px;
+    }
+    &>a:not(.mui-btn) {
+      padding: 5px;
+    }
+    .mui-media-body {
+      font-size: 13px;
+      margin-top: 2px;
+    }
+  }
+}
 </style>
